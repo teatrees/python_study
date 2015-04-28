b8e15bc6cf4fd2e994537ce6450110af893a4ee2
bbteatree <2xyjz2@163.com>
2015-04-28 16:59:29 +0800

    判断是否闰年：
    1.如果被400整除那就是闰年
    
    2.如果能被4整除且不能被100整除

--- lesson_01/07_year.py
+++ lesson_01/07_year.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+while True:
+    year = raw_input('please input year:')
+    if int(year) % 400 == 0:
+        print year
+        break
+    elif int(year) % 4 == 0 and int(year) %100 <>0:
+        print year
+        break
+    else:
+        print "This's year is not a leap year"
